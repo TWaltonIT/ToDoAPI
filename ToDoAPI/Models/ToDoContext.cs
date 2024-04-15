d6b7be6962808c91cdca05d0d6b19e040eb87a88
TWaltonIT <tww1998@yahoo.com>
2024-04-15 14:27:15 -0500

    Created and Configured API with CORS

---
+++ ToDoAPI/Models/ToDoContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ToDoAPI.Models
+{
+    public partial class ToDoContext : DbContext
+    {
+        public ToDoContext()
+        {
+        }
+
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<ToDo> ToDos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=ToDo;Trusted_Connection=true;MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CatDesc).HasMaxLength(100);
+
+                entity.Property(e => e.CatName).HasMaxLength(25);
+            });
+
+            modelBuilder.Entity<ToDo>(entity =>
+            {
+                entity.Property(e => e.ToDoId).HasColumnName("ToDoID");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.ToDos)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_ToDos_Categories");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
